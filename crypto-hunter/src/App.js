14559f9d86265921bde6d78755b3d4cd2ee0a2a0
SohamMore100 <soham.more16451@sakec.ac.in>
2024-10-21 18:27:57 +0530

    first commit

---
+++ crypto-hunter/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import { Header } from "./components/Header.js";
+function App() {
+  return (
+    <BrowserRouter>
+
+      <div>
+        <Header>
+
+        </Header>
+      </div>
+    
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
